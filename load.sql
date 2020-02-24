f492ed41407d2aec9e88765265e2b73679625e06
Surem19 <dloper02@gmail.com>
2020-02-24 13:00:50 +0100

    update
    add new params

--- load.sql
+++ load.sql
@@ -1,4 +1,4 @@
-CREATE database globalnodeeee; 
-create user postgresuserrrr with encrypted password 'd4ym72tv';
-grant all privileges on database globalnodeeee to postgresuserrrr;
+CREATE database globalnode; 
+create user postgresuser with encrypted password 'd4ym72tv';
+grant all privileges on database globalnode to postgresuser;
 
